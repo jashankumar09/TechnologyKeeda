caf968497bfb43003bc4e9c500689cc932172105
JashanKumarLng <jashan.kumar@lngconsultancy.co.in>
2024-03-11 10:46:53 +0530

    Repositories Patttern:- Added entities,Repositories,UI layer

---
+++ TechnologyKeeda.Entities/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnologyKeeda.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Default city";
+
+        public int StateId { get; set; }
+        //Navigation property
+        public State State { get; set; } = new();
+
+    }
+}
---
+++ TechnologyKeeda.Entities/State..cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnologyKeeda.Entities
+{
+    public class State
+    {
+        //State have multiple cities
+
+        public int id { get; set; }
+        public string Name { get; set; } = "Default state";
+
+        public int CountryId {get; set; } //foreign key
+                                          //Navigation property
+        public Country Country { get; set; } = new();
+
+        public ICollection<City> Cities { get; set; } = new HashSet<City>();
+
+    }
+}
